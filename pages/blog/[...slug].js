bcb88526dd24ffe54fedd6ed2971ae5cd73efbb9
Dalton Okechukwu <chike911@yahoo.com>
2021-05-07 18:35:35 -0600

    testing sourcetree

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -2,8 +2,8 @@ import { useRouter } from "next/router";
 
 function BlogPostsPage() {
     const router = useRouter();
-    console.log(router.pathname);
-    console.log(router.query);
+    // console.log(router.pathname);
+    // console.log(router.query);
     return (
         <div>
             <h1>Blog posts appear here!</h1>
