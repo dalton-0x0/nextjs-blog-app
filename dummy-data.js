44f643bdf556a4e972544771fb9467f4738d7caf
Dalton Okechukwu <chike911@yahoo.com>
2021-05-14 18:33:38 -0600

    dummy data is somehow redunant now, using firebase rtdb

--- dummy-data.js
+++ dummy-data.js
@@ -4,27 +4,27 @@ const DUMMY_EVENTS = [
         title: "Programming for everyone",
         description:
             "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
-        location: "Somestreet 25, 12345 San Somewhereo",
+        location: "25 Somestreet, 12345 Somewhere City",
         date: "2021-05-12",
         image: "images/coding-event.jpg",
-        isFeatured: false,
+        isFeatured: true,
     },
     {
         id: "e2",
         title: "Networking for introverts",
         description:
             "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
-        location: "New Wall Street 5, 98765 New Work",
+        location: "5 New Wall Street, 98765 New Yorkshire",
         date: "2021-05-30",
         image: "images/introvert-event.jpg",
-        isFeatured: true,
+        isFeatured: false,
     },
     {
         id: "e3",
         title: "Networking for extroverts",
         description:
             "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
-        location: "My Street 12, 10115 Broke City",
+        location: "12 My Street, 10115 Brokelane Town",
         date: "2022-04-10",
         image: "images/extrovert-event.jpg",
         isFeatured: true,
