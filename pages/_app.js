aae6c831bc5c5e962a4349490e75d18385a7d94d
Dalton Okechukwu <chike911@yahoo.com>
2021-05-15 22:45:18 -0600

    using next/head in _app with meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Head from "next/head";
+
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+                <link rel="icon" href="/blog.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
 export default MyApp;
