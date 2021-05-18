49cbd3a1eefe4791091a68136285d56e2c3aae3f
Dalton Okechukwu <chike911@yahoo.com>
2021-05-17 21:46:57 -0600

    added Head and meta

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
@@ -16,6 +17,14 @@ function AllEventsPage(props) {
 
     return (
         <Fragment>
+            <Head>
+                <title>All Events</title>
+                <meta
+                    name="description"
+                    content="upcoming events for everyone"
+                />
+                {/* <link rel="icon" href="/blog.ico" /> */}
+            </Head>
             <EventsSearch onSearch={findEventsHandler} />
             <EventList items={events} />
         </Fragment>
