8aa07344e977c2f24e103afbacf2867a59b1b158
Dalton Okechukwu <chike911@yahoo.com>
2021-05-24 11:01:37 -0600

    wrapping app with NotificationContextProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,23 @@
 import Head from "next/head";
 
 import Layout from "../components/layout/layout";
+import { NotificationContextProvider } from "../store/notification-context";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <Layout>
-            <Head>
-                <meta
-                    name="viewport"
-                    content="initial-scale=1.0, width=device-width"
-                />
-                <link rel="icon" href="/blog.ico" />
-            </Head>
-            <Component {...pageProps} />
-        </Layout>
+        <NotificationContextProvider>
+            <Layout>
+                <Head>
+                    <meta
+                        name="viewport"
+                        content="initial-scale=1.0, width=device-width"
+                    />
+                    <link rel="icon" href="/blog.ico" />
+                </Head>
+                <Component {...pageProps} />
+            </Layout>
+        </NotificationContextProvider>
     );
 }
 
